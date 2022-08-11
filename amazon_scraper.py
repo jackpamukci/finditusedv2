ffb5f0f607954792a8b656a64dc133f3ac220c75
Jack Pamukci <jackpamukci824@gmail.com>
2022-08-10 22:28:42 -0400

    first commit - selenium + python setup

---
+++ amazon_scraper.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+
+import requests
+import json
+import time
+
+def search_amazon():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.amazon.com/FIFA-22-PlayStation-4/dp/B098KVCFM8')
+    driver.close()
+
+search_amazon()
