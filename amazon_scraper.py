b8f11d471ede1a0cfd51171bfdd03f9d33cb21dd
Jack Pamukci <jackpamukci824@gmail.com>
2022-08-10 23:08:02 -0400

    first commit w selenium

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -6,6 +6,8 @@ import requests
 import json
 import time
 
+# comments for fuckery
+
 def search_amazon():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://www.amazon.com/FIFA-22-PlayStation-4/dp/B098KVCFM8')
