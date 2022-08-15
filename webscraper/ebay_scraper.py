c01fa5ded5a8a812af13c5cd8cae78a8f8b65018
Jack Pamukci <jackpamukci824@gmail.com>
2022-08-15 01:11:09 -0400

    added mercari functionality

--- webscraper/ebay_scraper.py
+++ webscraper/ebay_scraper.py
@@ -24,20 +24,33 @@ def search_ebay(item, kac):
     driver.get(url)
     driver.implicitly_wait(1)
 
-    title = driver.find_elements(By.CLASS_NAME, 's-item__title')
-    price = driver.find_elements(By.CLASS_NAME, 's-item__price')
-    picture = driver.find_elements(By.CLASS_NAME, 's-item__image-img')
-    link = driver.find_elements(By.CLASS_NAME, 's-item__link')
-
     data = []
 
-    for i in range(kac):
+    try:
+        title = driver.find_elements(By.CLASS_NAME, 's-item__title')
+        price = driver.find_elements(By.CLASS_NAME, 's-item__price')
+        picture = driver.find_elements(By.CLASS_NAME, 's-item__image-img')
+        link = driver.find_elements(By.CLASS_NAME, 's-item__link')
+
+        for i in range(kac):
+            data.append({
+                'title' : title[i + 1].text,
+                'price' : price[i + 1].text,
+                'picture' : picture[i + 1].get_attribute("src"),
+                'link' : link[i + 1].get_attribute("href")
+            })
+
+    except NoSuchElementException:
+        title, price, picture, link = 'no data found'
         data.append({
-            'title' : title[i + 1].text,
-            'price' : price[i + 1].text,
-            'picture' : picture[i + 1].get_attribute("src"),
-            'link' : link[i + 1].get_attribute("href")
+            'title' : title,
+            'price' : price,
+            'picture' : picture,
+            'link' : link
         })
 
     return data
+
+
+
     
